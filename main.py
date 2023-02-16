615e45a9486fd1b1381705da1cc64fc606cb7f56
DarkVIC <sirdarkvic@gmail.com>
2023-02-16 21:43:03 +0100

    Befor new list idea
    A save befor I try a new idea for merging

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import ast
 
 GAME_SIZE = 4
 
-playing_field = [[0 for y in range(GAME_SIZE)] for y in range(GAME_SIZE)]
+def create_field(size):
+    return [[0 for y in range(size)] for y in range(size)]
 
+
+playing_field = create_field(GAME_SIZE)
 
 
 def draw_playing_field(playing_field):
@@ -29,6 +32,7 @@ def draw_playing_field(playing_field):
     print()
 
 
+#? Compressing
 def compress_playfield(playing_field, compress_direction):
 
     print("Befor:")
@@ -54,20 +58,18 @@ def compress_playfield(playing_field, compress_direction):
 
 def one_move(playing_field, compress_direction, move_tile_x, move_tile_y):
 
-    x_test = move_tile_x
-    y_test = move_tile_y
-    #print(f"X: {x_test}, Y: {y_test}\n")
+    #print(f"X: {move_tile_x}, Y: {move_tile_y}\n")
 
     if compress_direction == "right":
-        if x_test + 1 > GAME_SIZE - 1:
-            #print(f"out of bounds x right\nX: {x_test}, Y: {y_test}\n")
+        if move_tile_x + 1 > GAME_SIZE - 1:
+            #print(f"out of bounds x right\nX: {move_tile_x}, Y: {move_tile_y}\n")
             return False
         
-        x_neighbour = playing_field[y_test][x_test + 1]
-        if x_neighbour == 0:
-            moved_value = playing_field[y_test][x_test]
-            playing_field[y_test][x_test] = 0
-            playing_field[y_test][x_test + 1] = moved_value
+        dir_neighbour = playing_field[move_tile_y][move_tile_x + 1]
+        if dir_neighbour == 0:
+            moved_value = playing_field[move_tile_y][move_tile_x]
+            playing_field[move_tile_y][move_tile_x] = 0
+            playing_field[move_tile_y][move_tile_x + 1] = moved_value
             return True
         
         else:
@@ -75,15 +77,15 @@ def one_move(playing_field, compress_direction, move_tile_x, move_tile_y):
 
 
     if compress_direction == "left":
-        if x_test - 1 < 0 :
-            #print(f"out of bounds x left\nX: {x_test}, Y: {y_test}\n")
+        if move_tile_x - 1 < 0 :
+            #print(f"out of bounds x left\nX: {move_tile_x}, Y: {move_tile_y}\n")
             return False
         
-        x_neighbour = playing_field[y_test][x_test - 1]
-        if x_neighbour == 0:
-            moved_value = playing_field[y_test][x_test]
-            playing_field[y_test][x_test] = 0
-            playing_field[y_test][x_test - 1] = moved_value
+        dir_neighbour = playing_field[move_tile_y][move_tile_x - 1]
+        if dir_neighbour == 0:
+            moved_value = playing_field[move_tile_y][move_tile_x]
+            playing_field[move_tile_y][move_tile_x] = 0
+            playing_field[move_tile_y][move_tile_x - 1] = moved_value
             return True
         
         else:
@@ -91,15 +93,15 @@ def one_move(playing_field, compress_direction, move_tile_x, move_tile_y):
 
 
     if compress_direction == "down":
-        if y_test + 1 > GAME_SIZE - 1:
-            #print(f"out of bounds y down\nX: {x_test}, Y: {y_test}\n")
+        if move_tile_y + 1 > GAME_SIZE - 1:
+            #print(f"out of bounds y down\nX: {move_tile_x}, Y: {move_tile_y}\n")
             return False
         
-        x_neighbour = playing_field[y_test + 1][x_test]
-        if x_neighbour == 0:
-            moved_value = playing_field[y_test][x_test]
-            playing_field[y_test][x_test] = 0
-            playing_field[y_test + 1][x_test] = moved_value
+        dir_neighbour = playing_field[move_tile_y + 1][move_tile_x]
+        if dir_neighbour == 0:
+            moved_value = playing_field[move_tile_y][move_tile_x]
+            playing_field[move_tile_y][move_tile_x] = 0
+            playing_field[move_tile_y + 1][move_tile_x] = moved_value
             return True
         
         else:
@@ -107,22 +109,126 @@ def one_move(playing_field, compress_direction, move_tile_x, move_tile_y):
 
 
     if compress_direction == "up":
-        if y_test - 1 < 0 :
-            #print(f"out of bounds y up\nX: {x_test}, Y: {y_test}\n")
+        if move_tile_y - 1 < 0 :
+            #print(f"out of bounds y up\nX: {move_tile_x}, Y: {move_tile_y}\n")
             return False
         
-        x_neighbour = playing_field[y_test - 1][x_test]
-        if x_neighbour == 0:
-            moved_value = playing_field[y_test][x_test]
-            playing_field[y_test][x_test] = 0
-            playing_field[y_test - 1][x_test] = moved_value
+        dir_neighbour = playing_field[move_tile_y - 1][move_tile_x]
+        if dir_neighbour == 0:
+            moved_value = playing_field[move_tile_y][move_tile_x]
+            playing_field[move_tile_y][move_tile_x] = 0
+            playing_field[move_tile_y - 1][move_tile_x] = moved_value
             return True
         
         else:
             return False
 
 
+#? Merging
+def merge_playfield(playing_field, merge_direction):
 
+    changed_playing_field = create_field(GAME_SIZE)
+
+    print("Befor:")
+    draw_playing_field(playing_field)
+
+    #while True:
+    merge_made_bool = False
+    for index_y, row in enumerate(playing_field):
+        for index_x, tile in enumerate(row):
+            if playing_field[index_y][index_x]:
+                merge_made_bool = one_merge(playing_field, merge_direction, index_x, index_y)
+        #     if merge_made_bool:
+        #         break
+        # if merge_made_bool:
+        #     break
+
+        # if not merge_made_bool:
+        #     break
+
+    if merge_direction == "t":
+        direction = input("Direction: ")
+        merge_tile_x = int(input("X: "))
+        merge_tile_y = int(input("Y: "))
+        one_merge(playing_field, direction, merge_tile_x, merge_tile_y)
+
+    print("After:")
+    draw_playing_field(playing_field)
+
+
+def one_merge(playing_field, merge_direction, merge_tile_x, merge_tile_y):
+
+    merge_value = playing_field[merge_tile_y][merge_tile_x]
+    #print(f"X: {merge_tile_x}, Y: {merge_tile_y}\nValue: {merge_value}\n")
+
+    if merge_direction == "right":
+        if merge_tile_x + 1 > GAME_SIZE - 1:
+            #print(f"out of bounds x right\nX: {merge_tile_x}, Y: {merge_tile_y}\n")
+            return False
+        
+        dir_neighbour = playing_field[merge_tile_y][merge_tile_x + 1]
+    
+        if dir_neighbour == merge_value:
+            playing_field[merge_tile_y][merge_tile_x] = 0
+            playing_field[merge_tile_y][merge_tile_x + 1] = merge_value + 1
+            return True
+        
+        else:
+            return False
+
+
+    if merge_direction == "left":
+        #print(f'Direction: {merge_direction}')
+        if merge_tile_x - 1 < 0 :
+            #print(f"out of bounds x left\nX: {merge_tile_x}, Y: {merge_tile_y}\n")
+            return False
+        
+        dir_neighbour = playing_field[merge_tile_y][merge_tile_x - 1]
+
+        if dir_neighbour == merge_value:
+            playing_field[merge_tile_y][merge_tile_x] = 0
+            playing_field[merge_tile_y][merge_tile_x - 1] = merge_value + 1
+            return True
+        
+        else:
+            return False
+
+
+    if merge_direction == "down":
+        #print(f'Direction: {merge_direction}')
+        if merge_tile_y + 1 > GAME_SIZE - 1:
+            #print(f"out of bounds y down\nX: {merge_tile_x}, Y: {merge_tile_y}\n")
+            return False
+        
+        dir_neighbour = playing_field[merge_tile_y + 1][merge_tile_x]
+
+        if dir_neighbour == merge_value:
+            playing_field[merge_tile_y][merge_tile_x] = 0
+            playing_field[merge_tile_y + 1][merge_tile_x] = merge_value + 1
+            return True
+        
+        else:
+            return False
+
+
+    if merge_direction == "up":
+        #print(f'Direction: {merge_direction}')
+        if merge_tile_y - 1 < 0 :
+            #print(f"out of bounds y up\nX: {merge_tile_x}, Y: {merge_tile_y}\n")
+            return False
+        
+        dir_neighbour = playing_field[merge_tile_y - 1][merge_tile_x]
+
+        if dir_neighbour == merge_value:
+            playing_field[merge_tile_y][merge_tile_x] = 0
+            playing_field[merge_tile_y - 1][merge_tile_x] = merge_value + 1
+            return True
+        
+        else:
+            return False
+
+
+#? Gameloop
 while True:
     command = input("Input a command: ")
     print()
@@ -148,7 +254,13 @@ while True:
 
 
     elif command == "merge" or command == "m":
-        pass 
+        merge_direction = input("Input a direction: ")
+
+        if merge_direction not in ["left", "right", "up", "down", "t"]:
+            print("Unknown direction! :(\n")
+            continue
+
+        merge_playfield(playing_field, merge_direction)
 
 
     elif command == "draw" or command == "d":
